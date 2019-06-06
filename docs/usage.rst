49b927247437cf3a3ac2d5b8e2bf0d20046e7603
Seonghyeon Kim <artoria@artoria.us>
2019-06-06 20:04:52 +0900

    RE-INIT

--- docs/usage.rst
+++
@@ -1,7 +0,0 @@
-=====
-Usage
-=====
-
-To use ostiarius in a project::
-
-    import ostiarius
--- ostiarius/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""Top-level package for ostiarius."""
-
-__author__ = """Seonghyeon Kim"""
-__email__ = 'seonghyeon@entrydsm.hs.kr'
-__version__ = '0.1.0'
