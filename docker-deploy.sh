71e3b1ddd04da2db65c8c9386ea629452712f365
Seonghyeon Kim <artoria@artoria.us>
2019-06-09 13:13:49 +0900

    Update docker-deploy.sh

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -4,11 +4,11 @@ version=`python -c "import ostiarius; print(ostiarius.__version__)"`
 
 echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin registry.entrydsm.hs.kr
 
-docker build -t registry.entrydsm.hs.kr/ostairius:${version} .
+docker build -t registry.entrydsm.hs.kr/ostiarius:${version} .
 
-docker tag registry.entrydsm.hs.kr/ostairius:${version} registry.entrydsm.hs.kr/ostairius:latest
+docker tag registry.entrydsm.hs.kr/ostiarius:${version} registry.entrydsm.hs.kr/ostiarius:latest
 
-docker push registry.entrydsm.hs.kr/ostairius:${version}
-docker push registry.entrydsm.hs.kr/ostairius:latest
+docker push registry.entrydsm.hs.kr/ostiarius:${version}
+docker push registry.entrydsm.hs.kr/ostiarius:latest
 
 exit
