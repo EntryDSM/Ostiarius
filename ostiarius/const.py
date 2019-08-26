7e22f96396f93c52ea02ba53230f89e24c3aa937
Jaehoon Kim <by09115@outlook.kr>
2019-08-26 22:40:13 +0900

    FIX: run environment, github token 환경변수 추가

--- ostiarius/const.py
+++ ostiarius/const.py
@@ -1,8 +1,13 @@
+from os import environ
+
 ROOT_ADMIN = "RootAdmin"
 ADMIN = "Admin"
 INTERVIEWER = "Interviewer"
 APPLICANT = "Applicant"
 
 HERMES = "http://hermes"
+
+RUN_ENV = environ.get("RUN_ENV")
+GITHUB_TOKEN = environ.get("GITHUB_TOKEN")
 
 UPLOAD_DIR = './pics'
