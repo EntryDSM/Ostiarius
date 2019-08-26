08ca6da4326dac4859f310efb812c07a3178ed30
Jaehoon Kim <by09115@outlook.kr>
2019-08-26 22:40:53 +0900

    ADD: Chanel의 jwt secret key를 불러오기 위해 Vault 적용

--- ostiarius/__main__.py
+++ ostiarius/__main__.py
@@ -4,8 +4,12 @@ import asyncio
 import uvloop
 
 from ostiarius.app import create_app
+from ostiarius.vault import VaultClient
 
 if __name__ == "__main__":
     asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+
+    VaultClient.init()
+
     app = create_app()
     app.run(host="0.0.0.0", port=7777, debug=os.getenv("DEBUG", False))
--- ostiarius/app.py
+++ ostiarius/app.py
@@ -6,14 +6,15 @@ from sanic.response import text, json
 from sanic_jwt_extended import JWTManager
 from sanic_cors import CORS
 
-from ostiarius.exceptions import ServiceUnavailable
 from ostiarius.gateway import bp
+from ostiarius.vault import settings
+from ostiarius.exceptions import ServiceUnavailable
 
 
 def create_app() -> Sanic:
     _app = Sanic("ostiarius")
 
-    _app.config['JWT_SECRET_KEY'] = "Chanel's secret key required"
+    _app.config['JWT_SECRET_KEY'] = settings.jwt_secret_key
     _app.config['RBAC_ENABLE'] = True
 
     JWTManager(_app)
